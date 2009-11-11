d2d81894be2d9a6bc2dcc63d70f9c8af891c77cc
gervandiepen <gervandiepen@6afa274c-0927-0410-9b47-a1284d797885>
2009-11-11 08:31:24 +0000

    Fixed typo
    git-svn-id: http://casacore.googlecode.com/svn/trunk@20811 6afa274c-0927-0410-9b47-a1284d797885

--- casa/tools/lib64linux.py
+++ casa/tools/lib64linux.py
@@ -2,7 +2,7 @@ import platform
 
 def get_libdir():
     if not platform.architecture()[0].startswith("64"):
-        Return "lib"
+        return "lib"
     dist = platform.dist()[0].lower()
     distdict = dict(suse='lib64', redhat='lib64')   
     return distdict.get(dist, 'lib')
