b55b891f562d78a8818d6b5f6add30a0d8130ac6
gervandiepen <gervandiepen@6afa274c-0927-0410-9b47-a1284d797885>
2009-11-11 08:33:56 +0000

    Make platform name lowercase
    git-svn-id: http://casacore.googlecode.com/svn/branches/single-scons@20812 6afa274c-0927-0410-9b47-a1284d797885

--- scons-tools/lib64linux.py
+++ scons-tools/lib64linux.py
@@ -3,6 +3,6 @@ import platform
 def get_libdir():
     if not platform.architecture()[0].startswith("64"):
         return "lib"
-    dist = platform.dist()[0]
+    dist = platform.dist()[0].lower()
     distdict = dict(suse='lib64', redhat='lib64')   
     return distdict.get(dist, 'lib')
