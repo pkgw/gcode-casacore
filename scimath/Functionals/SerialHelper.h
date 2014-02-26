d0cee493d6ca7d7d580726ab7594a99b944f6e9e
Peter Williams <peter@newton.cx>
2014-02-26 17:01:01 -0500

    TWEAK: fix exception specification thingie

--- scimath/Functionals/SerialHelper.h
+++ scimath/Functionals/SerialHelper.h
@@ -37,11 +37,11 @@ template<class T> class Array;
 
 template <class V>
 void getArrayVal(V &val, int type, const Record& gr, 
-                 const String& name, uInt index=0);
+		 const String& name, uInt index=0)WHATEVER_SUN_EXCEPTSPEC(InvalidSerializationError);
 
 template <class V>
 void getArray(Array<V> &val, int type, const Record& gr, 
-              const String& name);
+	      const String& name)WHATEVER_SUN_EXCEPTSPEC(InvalidSerializationError);
 
 // <summary>
 //
